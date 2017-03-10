bb3d9bd7237e7d04ab4e5e63773a02c86ac75f1a
Dave Kiss <dave@davekiss.com>
2017-03-10 14:14:22 -0800

    Restructure the product model

--- models/class-edd-mailchimp-product.php
+++ models/class-edd-mailchimp-product.php
@@ -3,55 +3,58 @@
 // Exit if accessed directly
 if ( ! defined( 'ABSPATH' ) ) exit;
 
-class EDD_Mailchimp_Product extends EDD_Mailchimp_Model {
+class EDD_MailChimp_Product extends EDD_MailChimp_Model {
 
-  public $download;
+  public $download = null;
 
-  public function __construct( $download ) {
-    if ( is_integer($download) ) {
-      $this->download = new EDD_Download($download)
-    } elseif ( is_object( $download ) && get_class($download) === 'EDD_Download' ) {
-      $this->download = $download;
-    } else {
-      $this->download = false;
-    }
+  public function __construct( $download = false ) {
+    parent::__construct();
+
+    $this->_set_download( $download );
+    $this->_build();
   }
 
   /**
-   * [create description]
-   * @return [type] [description]
+   * Assign the download which this product is associated with.
+   *
+   * @param mixed $list EDD_Download | int $download_id
    */
-  public function create() {
-    if ( ! $this->download ) {
-      return false;
+  protected function _set_download( $download ) {
+    if ( is_integer($download) ) {
+      $this->download = new EDD_Download($download);
+    } elseif ( is_object( $download ) && get_class($download) === 'EDD_Download' ) {
+      $this->download = $download;
     }
 
-    $product = array(
-      'id'          => $this->download->ID,
+    $this->id = apply_filters('edd.mailchimp.product.id', $this->download->ID, $this->download);
+  }
+
+
+  /**
+   * [build description]
+   * @return [type] [description]
+   */
+  protected function _build() {
+    $record = array(
+      'id'          => (string) $this->download->ID,
       'title'       => $this->download->post_title,
       'handle'      => $this->download->post_name, // slug
       'url'         => get_permalink( $this->download->id ),
       'description' => $this->download->post_excerpt,
       'type'        => $this->_get_category_name(),
-      'vendor'      => bloginfo('name'),
-      'variants'    => $variants,
+      'vendor'      => get_bloginfo('name'),
+      'variants'    => $this->_build_variants(),
       'published_at_foreign' => $this->download->post_date
     );
 
     // Add Product Image URL from Download Featured Image, if it exists.
     if (has_post_thumbnail( $this->download->ID ) ) {
       $image = wp_get_attachment_image_src( get_post_thumbnail_id( $this->download->ID ), 'single-post-thumbnail' );
-      $product['image_url'] = $image[0];
+      $this->_record['image_url'] = $image[0];
     }
 
-    $ProductsBatch = $api->new_batch();
-    $ProductsBatch->post("product_$i", 'ecommerce/stores/' . self::_get_store_id() . '/products', $product);
-    // The result includes a batch ID. At a later point, you can check the status of your batch:
-    $result = $ProductsBatch->execute();
-
-    // Check in on it.
-    $ProductsBatch = $api->new_batch($products_batch_id);
-    $result = $ProductsBatch->check_status();
+    $this->_record = apply_filters('edd.mailchimp.product', $record, $this->download);
+    return $this;
   }
 
   /**
@@ -85,17 +88,17 @@ class EDD_Mailchimp_Product extends EDD_Mailchimp_Model {
    */
   private function _build_variants() {
     $variants = array();
-    $sku = $download->get_sku();
+    $sku = $this->download->get_sku();
 
-    if ( $my_download->has_variable_prices() ) {
+    if ( $this->download->has_variable_prices() ) {
 
-      $variable_prices = $my_download->get_prices();
+      $variable_prices = $this->download->get_prices();
 
       foreach ( $variable_prices as $id => $data ) {
         $variant = array(
-          'id'    => $download->ID . '_' . $id,
+          'id'    => $this->download->ID . '_' . $id,
           'title' => $data['name'],
-          'url'   => get_permalink( $download->ID ),
+          'url'   => get_permalink( $this->download->ID ),
           'price' => $data['amount'],
         );
 
@@ -109,10 +112,10 @@ class EDD_Mailchimp_Product extends EDD_Mailchimp_Model {
     } else {
 
       $variant = array(
-        'id'    => (string) $download->ID . '_1',
-        'title' => $download->post_title,
-        'url'   => get_permalink( $download->ID ),
-        'price' => $download->get_price(),
+        'id'    => (string) $this->download->ID . '_1',
+        'title' => $this->download->post_title,
+        'url'   => get_permalink( $this->download->ID ),
+        'price' => $this->download->get_price(),
       );
 
       if ( $sku !== '-' ) {
