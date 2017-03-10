789622a8bc8eb415d488f15126c121d33e36b234
Dave Kiss <dave@davekiss.com>
2017-03-10 14:12:48 -0800

    Build out the parent model class

--- models/class-edd-mailchimp-model.php
+++ models/class-edd-mailchimp-model.php
@@ -3,29 +3,110 @@
 // Exit if accessed directly
 if ( ! defined( 'ABSPATH' ) ) exit;
 
-class EDD_Mailchimp_Model {
+class EDD_MailChimp_Model extends EDD_MailChimp_API {
 
-  public $api;
+  protected $_record = array();
 
-  public function __construct( $list_id ) {
-    $this->api = 'api';
+  /**
+   * Fetch all remote MailChimp records based on the
+   * called class name.
+   *
+   * @return [type] [description]
+   */
+  public static function all() {
+    $klass = new static;
+    return $klass->api->get( $klass->_endpoint, array( 'count' => 100 ) );
+  }
+
+
+  /**
+   * Find a single MailChimp record by ID based
+   * on the called class name.
+   *
+   * @return [type] [description]
+   */
+  public static function find( $id = '' ) {
+    $klass = new static;
+    return $klass->api->get( $this->_resource );
+  }
+
+
+  /**
+   * Check to see if an individual resource exists.
+   *
+   * @return bool
+   */
+  public function exists() {
+    $this->api->get( $this->_resource );
+    return $this->api->success();
+  }
+
+
+  /**
+   * Save the record.
+   *
+   * @return [type] [description]
+   */
+  public function save() {
+    if ( $this->exists() ) {
+      return $this->api->patch( $this->_resource, $this->_record );
+    } else {
+      return $this->api->post( $this->_endpoint, $this->_record );
+    }
   }
 
   /**
-   * [create description]
+   * Delete the record.
+   *
    * @return [type] [description]
    */
-  abstract public function create() {}
+  public function delete() {
+    $this->api->delete( $this->_resource );
+  }
+
 
   /**
-   * [update description]
+   * Record Getter
    * @return [type] [description]
    */
-  abstract public function update() {}
+  public function get_record() {
+    return $this->_record;
+  }
+
 
   /**
-   * [delete description]
-   * @return [type] [description]
+   * Check the property being called to determine if we're
+   * trying to return an associated object
+   *
+   * If it is a normal or cached property, just return it
+   *
+   * @param  string $name      Property name
+   * @param  mixed  $arguments passed arguments
+   * @return [type]            [description]
    */
-  abstract public function delete() {}
+  public function __get( $prop_name ) {
+
+    if ( array_key_exists( $prop_name, $this->_record ) ) {
+      return $this->_record[$prop_name];
+    }
+
+    if ( ! empty( $this->_has_many ) && array_key_exists( $prop_name, $this->_has_many ) ) {
+      $klass = new EDD_MailChimp_Collection($this, $prop_name);
+      return $klass;
+    }
+
+    return null;
+  }
+
+
+  /**
+   * Fetch all of a model's subresources if it
+   * has_many of the associated products.
+   */
+  public function __call( $method_name, $args ) {
+    if ( ! empty( $this->_has_many ) && array_key_exists( $method_name, $this->_has_many ) ) {
+      $klass = new EDD_MailChimp_Collection($this, $method_name);
+      return $klass->all();
+    }
+  }
 }
