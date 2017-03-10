7edd28ace09213dd48b515a0c48e155877163e52
Dave Kiss <dave@davekiss.com>
2017-03-10 14:13:51 -0800

    Structure out initial order model

--- models/class-edd-mailchimp-order.php
+++ models/class-edd-mailchimp-order.php
@@ -3,39 +3,41 @@
 // Exit if accessed directly
 if ( ! defined( 'ABSPATH' ) ) exit;
 
-class EDD_Mailchimp_Order extends EDD_Mailchimp_Model {
+class EDD_MailChimp_Order extends EDD_MailChimp_Model {
 
-  public $payment;
-  public $store;
+  protected $_payment;
+  public $_endpoint = 'orders';
 
-  public function __construct( $payment ) {
+  public function __construct( $payment = false ) {
+    parent::__construct();
+
     if ( is_integer($payment) ) {
-      $this->payment = new EDD_Payment($payment)
+      $this->_payment = new EDD_Payment($payment);
     } elseif ( is_object( $payment ) && get_class($payment) === 'EDD_Payment' ) {
-      $this->payment = $payment;
+      $this->_payment = $payment;
     } else {
-      $this->payment = false;
+      $this->_payment = false;
+    }
+
+    if ( $this->_payment ) {
+      $this->_resource = $this->_endpoint . '/' . $this->_payment->number;
     }
   }
 
   /**
-   * Create an order on MailChimp
-   *
-   * @see    http://developer.mailchimp.com/documentation/mailchimp/reference/ecommerce/stores/orders/
-   * @see    http://docs.easydigitaldownloads.com/article/1113-eddpayment
+   * [build description]
    * @return [type] [description]
    */
-  public function create() {
-
-    $order = array(
-      'id'       => (string) $this->payment->number,
+  protected function _build() {
+    $this->_record = array(
+      'id'       => (string) $this->_payment->number,
       'customer' => array(
-        'id'            => $this->payment->customer_id,
-        'email_address' => $this->payment->email,
+        'id'            => $this->_payment->customer_id,
+        'email_address' => $this->_payment->email,
         'opt_in_status' => false,
         // 'company'       => '',
-        'first_name'    => $this->payment->first_name,
-        'last_name'     => $this->payment->last_name,
+        'first_name'    => $this->_payment->first_name,
+        'last_name'     => $this->_payment->last_name,
         'orders_count'  => '',
         'total_spent'   => '',
         // 'address'       => array(
@@ -50,13 +52,14 @@ class EDD_Mailchimp_Order extends EDD_Mailchimp_Model {
         // )
       ),
       'campaign_id'          => '',
-      'financial_status'     => $this->payment->status_nicename,
-      'fulfillment_status'   => $this->payment->status_nicename,
-      'currency_code'        => $this->payment->currency,
-      'order_total'          => $this->payment->total,
-      'order_url'            => add_query_arg( 'id', $this->payment->ID, admin_url( 'edit.php?post_type=download&page=edd-payment-history&view=view-order-details' ) ),
-      'tax_total'            => $this->payment->tax,
-      'processed_at_foreign' => $this->payment->completed_date,
+      'financial_status'     => $this->_payment->status_nicename,
+      'fulfillment_status'   => $this->_payment->status_nicename,
+      'currency_code'        => $this->_payment->currency,
+      'order_total'          => $this->_payment->total,
+      'order_url'            => add_query_arg( 'id', $this->_payment->ID, admin_url( 'edit.php?post_type=download&page=edd-payment-history&view=view-order-details' ) ),
+      'tax_total'            => $this->_payment->tax,
+      'processed_at_foreign' => $this->_payment->completed_date,
+      'lines' => array(),
       // 'discount_total'       => '',
       // 'shipping_total'       => '',
       // 'tracking_code'        => '',
@@ -77,8 +80,8 @@ class EDD_Mailchimp_Order extends EDD_Mailchimp_Model {
       // ),
     );
 
-    foreach ( $this->payment->cart_details as $line ) {
-      $order['lines'][] = array(
+    foreach ( $this->_payment->cart_details as $line ) {
+      $this->_record['lines'][] = array(
         'id'         => $line['id'],
         'product_id' => $line['id'],
         'product_variant_id' => $line['item_number']['options']['price_id'],
@@ -87,7 +90,5 @@ class EDD_Mailchimp_Order extends EDD_Mailchimp_Model {
         'discount'   => $line['discount']
       );
     }
-
-    $api->post('ecommerce/stores/' . self::_get_store_id() . '/orders', $order);
   }
 }
