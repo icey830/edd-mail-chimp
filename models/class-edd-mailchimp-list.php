780b511cc73f4499135467a044bf0a3c37b4c854
Dave Kiss <dave@davekiss.com>
2017-03-17 16:24:52 -0700

    Refactor the Lists class

--- models/class-edd-mailchimp-list.php
+++ models/class-edd-mailchimp-list.php
@@ -5,22 +5,87 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 
 class EDD_MailChimp_List extends EDD_MailChimp_Model {
 
-	public $id;
 	protected $_endpoint = 'lists';
+	public $remote_id;
 
-	public function __construct( $list_id = '' ) {
+	public function __construct( $remote_list_id = '' ) {
 		parent::__construct();
 
-		if ( $list_id === '' ) {
-			$this->id = edd_get_option( 'eddmc_list', false );
-		} else {
-			$this->id = $list_id;
+		$this->remote_id = $remote_list_id;
+		$this->_resource = $this->_endpoint . '/' . $this->remote_id;
+		$this->_set_record();
+	}
+
+	/**
+	 * Fetch the connected list that is specified as default
+	 *
+	 *   $default_list = EDD_MailChimp_List::default();
+	 *
+	 * @return mixed
+	 */
+	public static function default() {
+		global $wpdb;
+
+		$result = $wpdb->get_row(
+			"SELECT * FROM $wpdb->edd_mailchimp_lists WHERE is_default = 1 LIMIT 1"
+		);
+
+		if ( $result ) {
+			return new static( $result->remote_id );
 		}
 
-		if ( $this->id ) {
-			$this->_resource = $this->_endpoint . '/' . $this->id;
-		}
+		return $result;
 	}
+
+
+	/**
+	 * Fetch all lists that have been connected locally.
+	 *
+	 *   $connected_lists = EDD_MailChimp_List::connected();
+	 *
+	 * @return mixed
+	 */
+	public static function connected() {
+		global $wpdb;
+		return $wpdb->get_results("SELECT * FROM $wpdb->edd_mailchimp_lists");
+	}
+
+
+	/**
+	 * Determine if this list has already been connected locally.
+	 *
+	 * @return boolean
+	 */
+	public function is_connected() {
+		return ! empty( $this->_record );
+	}
+
+
+	/**
+	 * Connect a list locally.
+	 *
+	 * @param  boolean $default [description]
+	 * @return [type]           [description]
+	 */
+	public function connect( $default = false ) {
+		global $wpdb;
+
+		$is_default = $default === true ? 1 : 0;
+
+		$wpdb->insert( $wpdb->edd_mailchimp_lists, array(
+			'remote_id'    => $this->remote_id,
+			'name'         => $this->name,
+			'is_default'   => $is_default,
+			'sync_status'  => 'pending',
+			'connected_at' => current_time('mysql')
+		), array(
+			'%s', '%s', '%d', '%s', '%s'
+		) );
+
+		$this->_set_record();
+		return $this;
+	}
+
 
 	/**
 	 * Subscribe an email to a list.
@@ -31,7 +96,7 @@ class EDD_MailChimp_List extends EDD_MailChimp_Model {
 		return;
 
 		// Make sure an API key and list ID has been entered
-		if ( empty( $this->api ) || ! $this->id ) {
+		if ( empty( $this->api ) || ! $this->remote_id ) {
 			return false;
 		}
 
@@ -63,6 +128,77 @@ class EDD_MailChimp_List extends EDD_MailChimp_Model {
 	}
 
 
+	/**
+	 * Fetch all of the associated interests for a connected list.
+	 *
+	 * @return array
+	 */
+	public function interests() {
+		global $wpdb;
+		$local_id = $this->_record->id;
+		return $wpdb->get_results("SELECT * FROM $wpdb->edd_mailchimp_interests WHERE list_id = $local_id");
+	}
+
+
+	/**
+	 * Sync a list's interests locally
+	 *
+	 * @return [type] [description]
+	 */
+	public function sync_interests() {
+		global $wpdb;
+
+		if ( ! $this->is_connected() ) {
+			return false;
+		}
+
+		$interest_categories = $this->get_remote_interests();
+
+		if ( empty( $interest_categories ) ) {
+			return;
+		}
+
+		foreach ( $interest_categories as $category ) {
+
+			foreach ( $category['interests'] as $interest ) {
+
+				$record = array(
+					'list_id' => $this->_record->id,
+					'interest_category_remote_id' => $category['id'],
+					'interest_category_name' => $category['name'],
+					'interest_remote_id' => $interest['id'],
+					'interest_name' => $interest['name'],
+				);
+
+				// Check if local interest record exists
+				$row = $wpdb->get_row( $wpdb->prepare(
+					"SELECT * FROM $wpdb->edd_mailchimp_interests WHERE interest_remote_id = %s",
+					$interest['id']
+				) );
+
+				if ( empty( $row ) ) {
+
+					// If not, insert a new record.
+					$wpdb->insert( $wpdb->edd_mailchimp_interests, $record, array(
+						'%d', '%s', '%s', '%s', '%s'
+					) );
+
+				} else {
+
+					// If it does, update it with any changed info
+					$wpdb->update(
+						$wpdb->edd_mailchimp_interests,
+						$record,
+						array( 'interest_remote_id' => $interest['id'] ),
+						array( '%d'),
+						array( '%s' )
+					);
+
+				}
+
+			}
+		}
+	}
 
 	/**
 	 * Fetch remote interests for a list.
@@ -106,6 +242,27 @@ class EDD_MailChimp_List extends EDD_MailChimp_Model {
 		}
 
 		return $all_category_data;
+	}
+
+
+	/**
+	 * Sets the class `_record` property if the list has been connected locally.
+	 *
+	 * @return empty array | object
+	 */
+	private function _set_record() {
+		global $wpdb;
+
+		$result = $wpdb->get_row( $wpdb->prepare(
+			"SELECT * FROM $wpdb->edd_mailchimp_lists WHERE remote_id = %s LIMIT 1",
+			$this->remote_id
+		) );
+
+		if ( $result !== null ) {
+			$this->_record = $result;
+		}
+
+		return $this;
 	}
 
 }
